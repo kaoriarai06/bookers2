befffb4886f1d44bbe8453421900bee0ff31b3ba
kaoriarai06 <132414421+kaoriarai06@users.noreply.github.com>
2023-06-18 13:05:40 +0000

    [add]投稿機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,15 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to book_path
+    redirect_to books_path
   end
   
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
