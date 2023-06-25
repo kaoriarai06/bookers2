850fe7d809958e0cb952a7e38972f758f7125fac
kaoriarai06 <132414421+kaoriarai06@users.noreply.github.com>
2023-06-25 09:25:38 +0000

    [update]編集ページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @book = Book.new
   end
 
   def edit
@@ -32,7 +33,7 @@ class UsersController < ApplicationController
   private
   
   def user_params
-    params.require(:user).permit(:name, :introduction, :image)
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
   
 end
